9c7797c749c1343612f64dcf4dba51900abd1b76
konovalov_k <konova10v@yandex.ru>
2022-12-02 12:50:01 +0500

    konova10v
    start

---
+++ testShop/testShop/testShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  testShopApp.swift
+//  testShop
+//
+//  Created by Кирилл Коновалов on 02.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct testShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
